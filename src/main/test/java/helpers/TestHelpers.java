28bb1f81a2b93174c10cf0375f449b8e85f4f8d1
shopComputer <BlyatBoio5@gmail.com>
2026-02-04 17:29:23 -0700

    Added tests for HubState and MatchState

---
+++ src/main/test/java/helpers/TestHelpers.java
@@ -0,0 +1,34 @@
+import bot.den.foxflow.RobotState;
+import edu.wpi.first.wpilibj.simulation.DriverStationSim;
+
+public class TestHelpers {
+    public static void setDriverStationState(RobotState state) {
+        switch(state) {
+            case DISABLED -> {
+                DriverStationSim.setEnabled(false);
+                DriverStationSim.setAutonomous(false);
+                DriverStationSim.setTest(false);
+            }
+            case AUTO -> {
+                DriverStationSim.setAutonomous(true);
+                DriverStationSim.setTest(false);
+                DriverStationSim.setEnabled(true);
+                DriverStationSim.setDsAttached(true);
+            }
+            case TELEOP -> {
+                DriverStationSim.setAutonomous(false);
+                DriverStationSim.setTest(false);
+                DriverStationSim.setEnabled(true);
+                DriverStationSim.setDsAttached(true);
+            }
+            case TEST -> {
+                DriverStationSim.setTest(true);
+                DriverStationSim.setAutonomous(false);
+                DriverStationSim.setEnabled(true);
+                DriverStationSim.setDsAttached(true);
+            }
+        }
+
+        DriverStationSim.notifyNewData();
+    }
+}
