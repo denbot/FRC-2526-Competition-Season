7a47ad3d23d1cbc4717868d54d71b265fd0fe4d5
shopComputer <BlyatBoio5@gmail.com>
2026-02-04 19:19:59 -0700

    Changed can IDs to match robot and Phoenix Tuner

--- src/main/java/frc/robot/subsystems/shooter/ShooterConstants.java
+++ src/main/java/frc/robot/subsystems/shooter/ShooterConstants.java
@@ -7,7 +7,7 @@ public final class ShooterConstants {
     public static final AngularAcceleration spinnerAcceleration = null;
     public static final AngularAcceleration kickerAcceleration = null;
     public static int LEFT_SPINNER_MOTOR_ID = 31;
-    public static int RIGHT_SPINNER_MOTOR_ID = 0;
+    public static int RIGHT_SPINNER_MOTOR_ID = 30;
     public static int KICKER_MOTOR_ID = 32;
     public static class OperatorConstants {
     public static final int kDriverControllerPort = 0;
