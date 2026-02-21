3db3c645837a7b27764515851eb734c4d5cf8a26
GucciBanana11 <rrkask@gmail.com>
2026-02-21 10:47:25 -0700

    Implemented atomic booleans and renamed variables

--- src/main/java/frc/robot/state/HopperState.java
+++ src/main/java/frc/robot/state/HopperState.java
@@ -10,24 +10,24 @@ public enum HopperState {
     RETRACTING,
     @DefaultState RETRACTED;
 
-    public static void setup(RebuiltStateMachine stateMachine, BooleanSupplier Button, BooleanSupplier RetractingLimitSwitch, BooleanSupplier DeployingLimitSwitch) {
+    public static void setup(RebuiltStateMachine stateMachine, BooleanSupplier button, BooleanSupplier retractingLimitSwitch, BooleanSupplier deployingLimitSwitch) {
         // Intake functions
         stateMachine
                 .state(HopperState.DEPLOYED)
                 .to(HopperState.RETRACTING)
-                .transitionWhen(Button); // Transition to retracting when bumper is pressed
+                .transitionWhen(button); // Transition to retracting when bumper is pressed
         stateMachine
                 .state(HopperState.RETRACTING)
                 .to(HopperState.RETRACTED)
-                .transitionWhen(RetractingLimitSwitch); // Transition to retracted when limit switch is activated
+                .transitionWhen(retractingLimitSwitch); // Transition to retracted when limit switch is activated
         stateMachine
                 .state(HopperState.RETRACTED)
                 .to(HopperState.DEPLOYING)
-                .transitionWhen(Button); // Transition to deploying when bumper is pressed
+                .transitionWhen(button); // Transition to deploying when bumper is pressed
         stateMachine
                 .state(HopperState.DEPLOYING)
                 .to(HopperState.DEPLOYED)
-                .transitionWhen(DeployingLimitSwitch); // Transition to deployed when limit switch is activated
+                .transitionWhen(deployingLimitSwitch); // Transition to deployed when limit switch is activated
 
     }
 }
--- src/test/java/frc/robot/state/HopperStateTest.java
+++ src/test/java/frc/robot/state/HopperStateTest.java
@@ -7,10 +7,17 @@ import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
+import java.util.concurrent.atomic.AtomicBoolean;
+
 import static frc.robot.helpers.TestHelpers.setDriverStationState;
 import static org.junit.jupiter.api.Assertions.*;
 
 public class HopperStateTest{
+
+    final AtomicBoolean button = new AtomicBoolean();
+    final AtomicBoolean retractingLimitSwitch = new AtomicBoolean();
+    final AtomicBoolean deployingLimitSwitch = new AtomicBoolean();
+
     @BeforeEach
     void setup() {
         assertTrue(HAL.initialize(500, 0));
@@ -28,7 +35,10 @@ public class HopperStateTest{
     @Test
     public void rightBumperDeploysHopper() {
         var machine = new RebuiltStateMachine();
-        HopperState.setup(machine, () -> true, () -> false, () -> false);
+        button.set(true);
+        retractingLimitSwitch.set(false);
+        deployingLimitSwitch.set(false);
+        HopperState.setup(machine, button::get, retractingLimitSwitch::get, deployingLimitSwitch::get);
         machine.poll();
         assertEquals(HopperState.DEPLOYING, machine.currentState().hopperState());
     }
@@ -36,9 +46,13 @@ public class HopperStateTest{
     @Test
     public void hopperDeploysAfterLimitSwitch() {
         var machine = new RebuiltStateMachine();
-        HopperState.setup(machine, () -> true, () -> false, () -> false);
+        button.set(true);
+        retractingLimitSwitch.set(false);
+        deployingLimitSwitch.set(false);
+        HopperState.setup(machine, button::get, retractingLimitSwitch::get, deployingLimitSwitch::get);
         machine.poll();
-        HopperState.setup(machine, () -> false, () -> false, () -> true);
+        button.set(false);
+        deployingLimitSwitch.set(true);
         machine.poll();
         assertEquals(HopperState.DEPLOYED, machine.currentState().hopperState());
     }
@@ -46,10 +60,15 @@ public class HopperStateTest{
     @Test
     public void rightBumperRetractsHopper() {
         var machine = new RebuiltStateMachine();
-        HopperState.setup(machine, () -> true, () -> false, () -> false);
+        button.set(true);
+        retractingLimitSwitch.set(false);
+        deployingLimitSwitch.set(false);
+        HopperState.setup(machine, button::get, retractingLimitSwitch::get, deployingLimitSwitch::get);
         machine.poll();
-        HopperState.setup(machine, () -> false, () -> false, () -> true);
+        button.set(false);
+        deployingLimitSwitch.set(true);
         machine.poll();
+        button.set(true);
         HopperState.setup(machine, () -> true, () -> false, () -> true);
         machine.poll();
         assertEquals(HopperState.RETRACTING, machine.currentState().hopperState());
@@ -58,12 +77,19 @@ public class HopperStateTest{
     @Test
     public void hopperRetractsAfterLimitSwitch() {
         var machine = new RebuiltStateMachine();
-        HopperState.setup(machine, () -> true, () -> false, () -> false);
+        button.set(true);
+        retractingLimitSwitch.set(false);
+        deployingLimitSwitch.set(false);
+        HopperState.setup(machine, button::get, retractingLimitSwitch::get, deployingLimitSwitch::get);
         machine.poll();
-        HopperState.setup(machine, () -> false, () -> false, () -> true);
+        button.set(false);
+        deployingLimitSwitch.set(true);
         machine.poll();
-        HopperState.setup(machine, () -> true, () -> false, () -> true);
+        button.set(true);
         machine.poll();
+        button.set(false);
+        retractingLimitSwitch.set(true);
+        deployingLimitSwitch.set(false);
         HopperState.setup(machine, () -> false, () -> true, () -> false);
         machine.poll();
         assertEquals(HopperState.RETRACTED, machine.currentState().hopperState());
