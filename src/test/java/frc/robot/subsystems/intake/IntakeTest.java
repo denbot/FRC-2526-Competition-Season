c9441512a4b98feebb471ae6f5b4226d39e7b225
GucciBanana11 <rrkask@gmail.com>
2026-02-23 16:45:19 -0700

    Intake test framework created

--- src/test/java/frc/robot/subsystems/intake/IntakeTest.java
+++ src/test/java/frc/robot/subsystems/intake/IntakeTest.java
@@ -1,5 +1,42 @@
 package frc.robot.subsystems.intake;
 
+import bot.den.foxflow.RobotState;
+import edu.wpi.first.hal.HAL;
+import edu.wpi.first.wpilibj2.command.CommandScheduler;
+import frc.robot.state.IntakeState;
+import frc.robot.state.RebuiltStateMachine;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import java.util.concurrent.atomic.AtomicBoolean;
+
+import static frc.robot.helpers.TestHelpers.setDriverStationState;
+import static org.junit.jupiter.api.Assertions.*;
+
 public class IntakeTest {
 
+    final AtomicBoolean leftTrigger = new AtomicBoolean();
+    final AtomicBoolean xButton = new AtomicBoolean();
+    private RebuiltStateMachine machine;
+    private Intake intake;
+
+    @BeforeEach
+    void setup() {
+        assertTrue(HAL.initialize(500, 0));
+
+        setDriverStationState(RobotState.DISABLED);
+
+        machine = new RebuiltStateMachine();
+        intake = new Intake(new IntakeIOSim(), machine);
+        leftTrigger.set(false);
+        xButton.set(false);
+    }
+
+    @AfterEach
+    public void cleanup() {
+        // This method runs after each test to reset the scheduler state
+        CommandScheduler.getInstance().cancelAll();
+        CommandScheduler.getInstance().run(); // Call run() to execute end() methods
+    }
 }
