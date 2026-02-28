a5e787b2f0fb7f0335690e71af8b442989733194
Stefan <BlyatBoio5@gmail.com>
2026-02-28 14:32:45 -0700

    Fixed shooter clamping and fixed teleop shooting only when spun up

--- src/main/java/frc/robot/subsystems/shooter/Shooter.java
+++ src/main/java/frc/robot/subsystems/shooter/Shooter.java
@@ -48,7 +48,7 @@ public class Shooter extends SubsystemBase{
 
         SmartDashboard.putNumber("Distance From Hub (Meters)", distance.magnitude());
         double x = distance.in(Meters); 
-        return RotationsPerSecond.of((Math.min(80, Math.pow(x, 2) *0.893293) + (1.75793 * x) + 40.677)).plus(spinnerVelocityOffset); // TODO This function is guesswork and estimation
+        return RotationsPerSecond.of((Math.min(80, Math.pow(x, 2) *0.893293 + (1.75793 * x) + 40.677))).plus(spinnerVelocityOffset); // TODO This function is guesswork and estimation
     }
 
     //TODO move this to be implemented in the runSpinner command, refference intake.java for example
