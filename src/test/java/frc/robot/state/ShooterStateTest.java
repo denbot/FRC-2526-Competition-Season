eb6f649655bb60d020f825ee53e869f3b146d954
GucciBanana11 <rrkask@gmail.com>
2026-02-28 14:20:28 -0700

    Added tests for shooter state

--- src/test/java/frc/robot/state/ShooterStateTest.java
+++ src/test/java/frc/robot/state/ShooterStateTest.java
@@ -5,11 +5,16 @@ import edu.wpi.first.hal.HAL;
 import edu.wpi.first.wpilibj2.command.CommandScheduler;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
 
+import java.util.List;
 import java.util.concurrent.atomic.AtomicBoolean;
 
 import static frc.robot.helpers.TestHelpers.setDriverStationState;
-import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.junit.jupiter.api.Assertions.*;
 
 public class ShooterStateTest {
     final AtomicBoolean rightBumper = new AtomicBoolean();
@@ -33,5 +38,52 @@ public class ShooterStateTest {
         // This method runs after each test to reset the scheduler state
         CommandScheduler.getInstance().cancelAll();
         CommandScheduler.getInstance().run(); // Call run() to execute end() methods
+    }
+
+    @ParameterizedTest
+    @MethodSource("buttonCombinations")
+    void shooterSpinsUpWhenRightBumperOrYButtonHeld(boolean rightBumper, boolean yButton) {
+        this.rightBumper.set(rightBumper);
+        this.yButton.set(yButton);
+        machine.poll();
+
+        assertEquals(ShooterState.SPINNING_UP, machine.currentState().shooterState());
+    }
+
+    @ParameterizedTest
+    @MethodSource("buttonCombinations")
+    void shooterDoesntStopWhenButtonsNotReleased(boolean rightBumper, boolean yButton) {
+        this.rightBumper.set(true);
+        this.yButton.set(true);
+        machine.poll();
+
+        // Double-check spinner is spinning up
+        assertEquals(ShooterState.SPINNING_UP, machine.currentState().shooterState());
+
+        this.rightBumper.set(rightBumper);
+        this.yButton.set(yButton);
+        assertNotEquals(ShooterState.STOPPED, machine.currentState().shooterState());
+    }
+
+    @ParameterizedTest
+    @MethodSource("buttonCombinations")
+    void shooterStopsWhenAllButtonsReleased(boolean rightBumper, boolean yButton) {
+        this.rightBumper.set(rightBumper);
+        this.yButton.set(yButton);
+        machine.poll();
+
+        // Double-check spinner is spinning up
+        assertEquals(ShooterState.SPINNING_UP, machine.currentState().shooterState());
+
+        this.rightBumper.set(false);
+        this.yButton.set(false);
+        assertEquals(ShooterState.STOPPED, machine.currentState().shooterState());
+    }
+
+    private static List<Arguments> buttonCombinations() {
+        return List.of(
+                Arguments.arguments(true, false),
+                Arguments.arguments(false, true),
+                Arguments.arguments(true, true));
     }
 }
