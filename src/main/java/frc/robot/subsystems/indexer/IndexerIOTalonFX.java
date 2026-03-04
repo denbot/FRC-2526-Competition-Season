e4eeeaf592974c07cfe9809c61a7d40fdd4cc7d1
Justin Nesselrotte <admin@jnesselr.org>
2026-03-04 03:29:57 -0700

    Calling optimize bus utilization method

--- src/main/java/frc/robot/subsystems/indexer/IndexerIOTalonFX.java
+++ src/main/java/frc/robot/subsystems/indexer/IndexerIOTalonFX.java
@@ -11,6 +11,7 @@ import com.ctre.phoenix6.configs.Slot0Configs;
 import com.ctre.phoenix6.configs.TalonFXConfiguration;
 import com.ctre.phoenix6.controls.CoastOut;
 import com.ctre.phoenix6.controls.VelocityVoltage;
+import com.ctre.phoenix6.hardware.ParentDevice;
 import com.ctre.phoenix6.hardware.TalonFX;
 import com.ctre.phoenix6.signals.FeedbackSensorSourceValue;
 import com.ctre.phoenix6.signals.InvertedValue;
@@ -53,12 +54,13 @@ public class IndexerIOTalonFX implements IndexerIO{
 
         indexMotor.setNeutralMode(NeutralModeValue.Coast);
         tryUntilOk(5, () -> indexMotor.getConfigurator().apply(indexMotorConfig, 0.25));
-
         
         BaseStatusSignal.setUpdateFrequencyForAll(
-            indexMotor.getIsProLicensed().getValue() ? 200 : 50, 
+            50,
             indexMotorVelocity, 
             indexMotorCurrentAmps);
+
+        ParentDevice.optimizeBusUtilizationForAll(indexMotor);
     }
 
     @Override
