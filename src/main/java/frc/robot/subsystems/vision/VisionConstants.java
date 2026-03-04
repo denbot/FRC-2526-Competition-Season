43ab81895a1f7a3e9376a2633c11d6a09111285b
Justin Nesselrotte <admin@jnesselr.org>
2026-03-03 18:22:23 -0700

    RobotContainer new correctly instatiates the vision subsystem

--- src/main/java/frc/robot/subsystems/vision/VisionConstants.java
+++ src/main/java/frc/robot/subsystems/vision/VisionConstants.java
@@ -17,22 +17,25 @@ public class VisionConstants {
     public static AprilTagFieldLayout aprilTagLayout =
             AprilTagFieldLayout.loadField(AprilTagFields.kDefaultField);
 
-    // Camera names, must match names configured on coprocessor
-    public static String camera0Name = "camera_0";
-    public static String camera1Name = "camera_1";
+    // Camera names must match names configured on coprocessor
+    public static String cameraLeftName = "limelight-left";
+    public static String cameraRightName = "limelight-right";
+    public static String cameraFrontName = "limelight-front";
 
-    // Robot to camera transforms
+    // TODO Robot to camera transforms
     // (Not used by Limelight, configure in web UI instead)
-    public static Transform3d robotToCamera0 =
+    public static Transform3d robotToCameraLeft =
             new Transform3d(0.2, 0.0, 0.2, new Rotation3d(0.0, -0.4, 0.0));
-    public static Transform3d robotToCamera1 =
+    public static Transform3d robotToCameraRight =
+            new Transform3d(-0.2, 0.0, 0.2, new Rotation3d(0.0, -0.4, Math.PI));
+    public static Transform3d robotToCameraFront =
             new Transform3d(-0.2, 0.0, 0.2, new Rotation3d(0.0, -0.4, Math.PI));
 
     // Basic filtering thresholds
     public static double maxAmbiguity = 0.3;
     public static double maxZError = 0.75;
 
-    // Standard deviation baselines, for 1 meter distance and 1 tag
+    // Standard deviation baselines, for 1-meter distance and 1 tag
     // (Adjusted automatically based on distance and # of tags)
     public static double linearStdDevBaseline = 0.02; // Meters
     public static double angularStdDevBaseline = 0.06; // Radians
