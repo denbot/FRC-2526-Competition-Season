5d095f288e212adf8ffd51cffed7485ffecfca09
Justin Nesselrotte <admin@jnesselr.org>
2026-03-03 17:43:31 -0700

    Switched to Advantage Kit vision template

--- src/main/java/frc/robot/subsystems/vision/LimelightConstants.java
+++
@@ -1,7 +0,0 @@
-package frc.robot.subsystems.vision;
-
-public class LimelightConstants {
-    public static final String backLeftLimelightIP = "10.95.86.11";
-    public static final String backRightLimelightIP = "10.95.86.12";
-    public static final String frontLimelightIP = "10.95.86.13";
-}
