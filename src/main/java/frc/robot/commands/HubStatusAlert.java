a0292dcf295c7c052fe21cad6897ad189e8ed7e0
GucciBanana7 <rrkask@gmail.com>
2026-04-06 17:01:52 -0600

    10 seconds

--- src/main/java/frc/robot/commands/HubStatusAlert.java
+++ src/main/java/frc/robot/commands/HubStatusAlert.java
@@ -38,7 +38,7 @@ public class HubStatusAlert extends Command {
             } else {
                 badDataAlert.set(true);
             }
-        } else if (timer.hasElapsed(Seconds.of(2))) {
+        } else if (timer.hasElapsed(Seconds.of(10))) {
             emptyStatusAlert.set(true);
         }
     }
